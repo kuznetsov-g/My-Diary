3b10d711ba33d364588eda8b72fc888e2e72ea32
George <backs159@icloud.com>
2023-04-18 16:38:36 -0700

    Initial Commit

---
+++ My Diary/My_DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_DiaryApp.swift
+//  My Diary
+//
+//  Created by Георгий Кузнецов on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct My_DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
